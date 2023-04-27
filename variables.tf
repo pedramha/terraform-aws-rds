b2d073b576db874c05216d3edb7150a4631aac58
Pedram Hamidehkhan <pedram.hamidehkhan@gmail.com>
2023-04-27 10:37:06 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,6 +2,13 @@ variable "db_name" {
   description = "Unique name to assign to RDS instance"
 }
 
+variable "stage" {
+  description = "stage"
+}
+
+variable "region" {
+  description = "region"
+}
 variable "db_username" {
   description = "RDS root username"
 }
